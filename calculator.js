8145a8e47757e281b40662e4ca5cee056838f286
priest davos <rajjo1920london@gmail.com>
2023-12-16 02:52:37 +0530

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,8 @@
+// document.querySelector('button').textContent="Deaead"
+let currentValue='77';
+let expression=''
+let final_expression=[]
+//console.log(document.querySelectorAll('button').
+display=document.querySelector('.display');
+//document.querySelector('.refresh-history-btn').value="zd"
+// display.value=currentValue;
